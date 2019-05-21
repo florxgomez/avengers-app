614d53a0c3facaa01d77546cc05a24a00cee2a35
Florencia Gomez <Florxgomez@gmail.com>
2019-05-21 15:19:27 -0300

    organizing files

---
+++ containers/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import { images } from "../public/images";
+import { Scroll } from "../components/Scroll";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      images: [],
+      searchField: ""
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ images: images });
+  }
+
+  onSearchChange = e => {
+    this.setState({ searchField: e.target.value });
+  };
+  render() {
+    const { images, searchField } = this.state;
+    const filteredAvengers = images.filter(image => {
+      return (
+        image.avenger.toLowerCase().includes(searchField.toLowerCase()) ||
+        image.name.toLowerCase().includes(searchField.toLowerCase())
+      );
+    });
+    return !images.length ? (
+      <h1>Loading...</h1>
+    ) : (
+      <div className="tc">
+        <img
+          className="pt2"
+          src="https://fontmeme.com/permalink/190518/ac068f97191bc097a0ae3494c6ec34fe.png"
+          alt="avengers-infinity-war-font"
+          border="0"
+        />
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <CardList images={filteredAvengers} />
+        </Scroll>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/Scroll.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Scroll = (props) => {
-	return props.children;
-};
-
-export default Scroll;
