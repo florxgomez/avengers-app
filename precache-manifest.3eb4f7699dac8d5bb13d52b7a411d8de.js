2618aad61f15672826e170afa51384e038416554
Flor <florxgomez@gmail.com>
2019-05-21 18:04:07 -0300

    Updates

---
+++ precache-manifest.3eb4f7699dac8d5bb13d52b7a411d8de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77b699bbe02afe20ddd6ceb2cd7cb5ab",
+    "url": "/avengers-app/index.html"
+  },
+  {
+    "revision": "6f6d9243dc53ff0713d5",
+    "url": "/avengers-app/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "6498c57674cb41751b05",
+    "url": "/avengers-app/static/css/main.8be5bad4.chunk.css"
+  },
+  {
+    "revision": "6f6d9243dc53ff0713d5",
+    "url": "/avengers-app/static/js/2.2d717d87.chunk.js"
+  },
+  {
+    "revision": "6498c57674cb41751b05",
+    "url": "/avengers-app/static/js/main.c101d127.chunk.js"
+  },
+  {
+    "revision": "19998bd0819dfbdc8410",
+    "url": "/avengers-app/static/js/runtime~main.5834b226.js"
+  }
+]);
